c3d9cfaa11bd8d930ea993ae476718d81860bf8d
KWass1112 <57112616+KWass1112@users.noreply.github.com>
2019-11-21 15:14:21 -0600

    MadLibs

---
+++ MadLibsGUI/MadLibsGUI/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace MadLibsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtWordEndingInEst = new System.Windows.Forms.TextBox();
+            this.txtBodyPartPlural = new System.Windows.Forms.TextBox();
+            this.txtAnimal = new System.Windows.Forms.TextBox();
+            this.txtNoun = new System.Windows.Forms.TextBox();
+            this.txtPluralNoun = new System.Windows.Forms.TextBox();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnMixItUp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(156, 95);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(100, 20);
+            this.txtColor.TabIndex = 0;
+            // 
+            // txtWordEndingInEst
+            // 
+            this.txtWordEndingInEst.Location = new System.Drawing.Point(156, 148);
+            this.txtWordEndingInEst.Name = "txtWordEndingInEst";
+            this.txtWordEndingInEst.Size = new System.Drawing.Size(100, 20);
+            this.txtWordEndingInEst.TabIndex = 1;
+            // 
+            // txtBodyPartPlural
+            // 
+            this.txtBodyPartPlural.Location = new System.Drawing.Point(156, 200);
+            this.txtBodyPartPlural.Name = "txtBodyPartPlural";
+            this.txtBodyPartPlural.Size = new System.Drawing.Size(100, 20);
+            this.txtBodyPartPlural.TabIndex = 2;
+            // 
+            // txtAnimal
+            // 
+            this.txtAnimal.Location = new System.Drawing.Point(156, 245);
+            this.txtAnimal.Name = "txtAnimal";
+            this.txtAnimal.Size = new System.Drawing.Size(100, 20);
+            this.txtAnimal.TabIndex = 3;
+            // 
+            // txtNoun
+            // 
+            this.txtNoun.Location = new System.Drawing.Point(339, 95);
+            this.txtNoun.Name = "txtNoun";
+            this.txtNoun.Size = new System.Drawing.Size(100, 20);
+            this.txtNoun.TabIndex = 4;
+            // 
+            // txtPluralNoun
+            // 
+            this.txtPluralNoun.Location = new System.Drawing.Point(340, 148);
+            this.txtPluralNoun.Name = "txtPluralNoun";
+            this.txtPluralNoun.Size = new System.Drawing.Size(100, 20);
+            this.txtPluralNoun.TabIndex = 5;
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(340, 200);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber1.TabIndex = 6;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(339, 249);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber2.TabIndex = 7;
+            // 
+            // btnMixItUp
+            // 
+            this.btnMixItUp.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMixItUp.Location = new System.Drawing.Point(240, 310);
+            this.btnMixItUp.Name = "btnMixItUp";
+            this.btnMixItUp.Size = new System.Drawing.Size(104, 46);
+            this.btnMixItUp.TabIndex = 8;
+            this.btnMixItUp.Text = "Mix it up!!";
+            this.btnMixItUp.UseVisualStyleBackColor = true;
+            this.btnMixItUp.Click += new System.EventHandler(this.btnMixItUp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(110, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Color:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Word ending in est:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Body part plural:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(96, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Animal:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(298, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Noun:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(262, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Plural noun:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(285, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Number:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(285, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Number:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.Control;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("Mongolian Baiti", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(146, 359);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(310, 105);
+            this.lblResult.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Old English Text MT", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(171, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(215, 48);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "The Dragon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(551, 511);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMixItUp);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.txtPluralNoun);
+            this.Controls.Add(this.txtNoun);
+            this.Controls.Add(this.txtAnimal);
+            this.Controls.Add(this.txtBodyPartPlural);
+            this.Controls.Add(this.txtWordEndingInEst);
+            this.Controls.Add(this.txtColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtWordEndingInEst;
+        private System.Windows.Forms.TextBox txtBodyPartPlural;
+        private System.Windows.Forms.TextBox txtAnimal;
+        private System.Windows.Forms.TextBox txtNoun;
+        private System.Windows.Forms.TextBox txtPluralNoun;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnMixItUp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ MadLibsGUI/MadLibsGUI/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MadLibsGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMixItUp_Click(object sender, EventArgs e)
+        {
+            double num;
+            num = Convert.ToDouble(txtNumber1.Text) - Convert.ToDouble(txtNumber2.Text);
+
+            lblResult.Text = "The " + txtColor.Text + " dragon is the " + txtWordEndingInEst.Text + " dragon of all. It has "
+                + num + " " + txtBodyPartPlural.Text + ", and a "
+                + txtAnimal.Text + " shapped like a " + txtNoun.Text + ", although it will feast on nearly anything.";
+        }
+
+    }
+}
